a1dc38271e1d08ed05c991a9033e7cfb36306a1b
Vladyslav Ksendziuk <vlad.borland13@gmail.com>
2023-02-04 14:10:43 -0600

    Added first Cucumber Test Case

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -1,12 +1,9 @@
 package steps;
 
 import cucumber.api.java.After;
-import cucumber.api.java.Before;
-import org.openqa.selenium.WebDriver;
 import utils.Driver;
 
 public class Hooks {
-
 
     @After
     public void teardown(){
